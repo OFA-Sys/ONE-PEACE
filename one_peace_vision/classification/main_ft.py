b6cdb0499792eb939ca15b38e2ae1de52f643d2f
simonJJJ <821898965@qq.com>
2023-05-28 21:52:10 +0800

    fix

--- one_peace_vision/classification/main_ft.py
+++ one_peace_vision/classification/main_ft.py
@@ -167,6 +167,7 @@ def get_args():
     parser.add_argument('--world_size', default=1, type=int,
                         help='number of distributed processes')
     parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', action='store_true')
     parser.add_argument('--dist_url', default='env://',
                         help='url used to set up distributed training')
 
