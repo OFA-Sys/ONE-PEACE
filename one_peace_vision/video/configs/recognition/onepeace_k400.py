7db3413c68821f7d1f47ebb74c593e4100eb01f3
simonJJJ <821898965@qq.com>
2023-06-19 00:36:29 +0800

    fix config path

--- one_peace_vision/video/configs/recognition/onepeace_k400.py
+++ one_peace_vision/video/configs/recognition/onepeace_k400.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../../_base_/models/onepeace.py', '../../_base_/default_runtime.py'
+    '../_base_/models/onepeace.py', '../_base_/default_runtime.py'
 ]
 # model settings
 model = dict(
